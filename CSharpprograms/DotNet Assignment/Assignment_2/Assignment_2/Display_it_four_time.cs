fb01fe424ff60ffb338d4959831ac45cde596ee8
Bhanuprakash Patapanchala <bhanuprakashp@infinite.com>
2024-11-06 11:16:31 +0530

    Assesment1

--- CSharpprograms/DotNet Assignment/Assignment_2/Assignment_2/Display_it_four_time.cs
+++ CSharpprograms/DotNet Assignment/Assignment_2/Assignment_2/Display_it_four_time.cs
@@ -8,5 +8,20 @@ namespace Assignment_2
 {
     class Display_it_four_time
     {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < 2; i++)
+            {
+                Console.WriteLine("{0} {0} {0} {0}", a);
+                Console.WriteLine("{0}{0}{0}{0}", a);
+            }
+            for (int j = 1; j < 2; j++)
+            {
+                Console.WriteLine("{0} {0} {0} {0}", a);
+                Console.WriteLine("{0}{0}{0}{0}", a);
+                Console.ReadLine();
+            }
+        }
     }
 }
--- CSharpprograms/DotNet Assignment/Assignment_2/Assignment_2/One_array to Another_array.cs
+++ CSharpprograms/DotNet Assignment/Assignment_2/Assignment_2/One_array to Another_array.cs
@@ -8,5 +8,17 @@ namespace Assignment_2
 {
     class One_array_to_Another_array
     {
+        static void Main()
+        {
+            int[] array_1 = new int[] { 1, 2, 3, 4, 5 };
+            int[] array_2 = array_1;
+            for (int i = 0; i < array_2.Length; i++)
+            {
+                Console.Write(array_2[i] + " ");
+
+            }
+
+            Console.ReadLine();
+        }
     }
 }
