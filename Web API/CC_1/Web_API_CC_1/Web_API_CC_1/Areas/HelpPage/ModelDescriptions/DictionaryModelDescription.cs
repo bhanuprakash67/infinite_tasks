9971c83f1e515f86fa2c5b00033f7181aa82c380
Bhanuprakash Patapanchala <bhanuprakashp@infinite.com>
2025-01-08 10:48:21 +0530

    WEB_API_CC1

---
+++ Web API/CC_1/Web_API_CC_1/Web_API_CC_1/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Web_API_CC_1.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Web API/CC_1/Web_API_CC_1/Web_API_CC_1/Controllers/CountryController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Web_API_CC_1.Models;
+namespace Web_API_CC_1.Controllers
+{
+    public class CountryController : ApiController
+    {
+
+        public List<Country> c = new List<Country>();
+        public CountryController()
+        {
+            c.Add(new Country { ID = 1, CountryName = "India", Capital = "Delhi" });
+            c.Add(new Country { ID = 2, CountryName = "SriLanka", Capital = "Colombo" });
+            c.Add(new Country { ID = 3, CountryName = "Japan", Capital = "Tokyo" });
+        }
+        [HttpGet]
+        public List<Country> GetallCountries()
+        {
+            return c;
+        }
+        [HttpPost]
+        public List<Country> Create([FromBody] Country country)
+        {
+            c.Add(country);
+            return c;
+        }
+        [HttpPut]
+        public List<Country> Update(int id, [FromBody] Country country)
+        {
+            var CountryList = c.Find(c => c.ID == id);
+            if (CountryList == null)
+            {
+            }
+            CountryList.CountryName = country.CountryName;
+            CountryList.Capital = country.Capital;
+            return c;
+        }
+        [HttpDelete]
+        public List<Country> Delete(int id)
+        {
+            var country_list = c.Find(c => c.ID == id);
+            if (country_list == null)
+            {
+            }
+            c.Remove(country_list);
+            return c;
+        }
+    }
+}
+
